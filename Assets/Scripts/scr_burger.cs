bb39ece459eaf083a6e50a03b1af4cb61647ecf8
junhaddi <36301491+junhaddi@users.noreply.github.com>
2018-07-20 15:49:11 +0900

    Add debug log

---
+++ Assets/Scripts/scr_burger.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_burger : MonoBehaviour {
+    private void Awake()
+    {
+        Debug.Log("GIT Test");
+    }
+}
